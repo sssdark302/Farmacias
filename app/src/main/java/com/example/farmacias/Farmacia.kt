21d99190ebf7c795f60c8e5ab29d74af580fb15c
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-12-04 18:34:07 +0100

    feat/dataclass

---
+++ app/src/main/java/com/example/farmacias/Farmacia.kt
@@ -0,0 +1,8 @@
+package com.example.farmacias
+
+data class Farmacia(
+    val nombre: String = "",
+    val telefono: String = "",
+    val latitud: Double = 0.0,
+    val longitud: Double = 0.0
+)
